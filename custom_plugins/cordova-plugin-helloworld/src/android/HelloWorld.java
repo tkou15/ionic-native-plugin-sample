97ba238c30409c4d7029aaf92891e4d0e66d7c50
koichi <tkou15.work@gmail.com>
2020-09-17 21:40:40 +0900

    update

--- custom_plugins/cordova-plugin-helloworld/src/android/HelloWorld.java
+++ custom_plugins/cordova-plugin-helloworld/src/android/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("sayHello")) {
+        if (action.equals("echo")) {
             String name = data.getString(0);
             String message = "Hello, World !!! " + "Hello, " + name;
             callbackContext.success(message);
